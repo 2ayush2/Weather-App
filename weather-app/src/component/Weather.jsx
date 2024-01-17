14c12d50094ac36097fdcd202891a204e6fe54e3
unknown <ayushkhadka410@gmail.com>
2024-01-17 23:49:15 +0545

    Weather app logic updated

---
+++ weather-app/src/component/Weather.jsx
@@ -0,0 +1,36 @@
+import { React, useState, useEffect } from "react";
+import "./weather.css";
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=b3ccb966709c45c892e44ead1b58daa8
+
+const Weather = () => {
+  const [input, setInput] = useState("null");
+  const [city, setCity] = useState("");
+  useEffect(() => {
+    const FetchApi = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=b3ccb966709c45c892e44ead1b58daa8`;
+      const fetchData = await fetch(url);
+      const jsonData = await fetchData.json();
+      setCity(jsonData.main);
+
+      console.log(jsonData);
+    };
+    FetchApi();
+  }, [input]);
+  return (
+    <>
+      <div className="inputData">
+        <input type="text" onChange={(event) => setInput(event.target.value)} />
+      </div>
+      {!city ? (
+        <p>Not found result</p>
+      ) : (
+        <div className="box">
+          <h1>{input}</h1>
+          <p>{city.temp}</p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Weather;
